59032981d543d413d3dc7ec3e078d9dcdf523223
Guo An Wang <you@example.com>
2025-11-29 23:17:14 -0800

    Initial commit

---
+++ Lab 36: Records BST/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  CS-210 | Lab 36: Records BST | Guo An Wang
+//  IDE:Xcode
+//  Created by Guo An Wang on 11/29/25.
+//
+
+#include <iostream>
+#include "IntBinaryTree.h"
+using namespace std;
+int main() {
+    return 0;
+}
